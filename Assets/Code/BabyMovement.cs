d9f75166a3c3e8cb54a3389e26947f5e4141c9ff
domiendemol <domiendemol@gmail.com>
2025-01-25 11:34:12 +0100

    free baby movement

--- Assets/Code/BabyMovement.cs
+++ Assets/Code/BabyMovement.cs
@@ -12,21 +12,25 @@ public class BabyMovement : MonoBehaviour
 		FREE,
 		PATH
 	}
+
+	[SerializeField] private float _speed = 3f;
+	[SerializeField] private float _rotationSpeed = 250f;
+	[SerializeField] private float _wallCheckDist = 3f;
 	
 	private List<Vector3> _linePoints = new List<Vector3>();
 	private LineRenderer _lineRenderer;
 	private bool _drawing = false;
 	private float _lastPointTime;
 	private int _pathIndex;
-	private float _speed = 5f;
 	private float _moveT = 0;
+	private float _rotationRemaining;
 	private MovementState _state = MovementState.FREE;
 	
 	
 	private void Awake()
 	{
 		_lineRenderer = GetComponent<LineRenderer>();
-		ChangeState(MovementState.NONE);
+		ChangeState(MovementState.FREE);
 	}
 
 	private void Update()
@@ -49,11 +53,16 @@ public class BabyMovement : MonoBehaviour
 
 			MoveOnPath();
 		}
+		else if (_state == MovementState.FREE)
+		{
+			MoveFreely();
+		}
 	}
 
 	private void ChangeState(MovementState newState)
 	{
 		_state = newState;
+		Debug.Log($"NEW STATE {newState}");
 	}
 
 	private Vector3 GetMouseWorldPosition(string layerName)
@@ -101,16 +110,72 @@ public class BabyMovement : MonoBehaviour
 			_moveT = 0;
 			dist = Vector3.Distance(transform.position, _linePoints[_pathIndex]);
 		}
+		
+		if (_pathIndex == _linePoints.Count - 1 && !_drawing) ChangeState(MovementState.FREE);
+		
 		float segmentDistance = Vector3.Distance(_linePoints[_pathIndex-1], _linePoints[_pathIndex]);
 		float totalSegmentTime = segmentDistance / _speed; 
 		
-		
 		_moveT += Time.deltaTime;
-		// Debug.Log($"Moving to point: {_pathIndex}, distance: {dist}, segment: {segmentDistance}, t: {_moveT}");
 
 		transform.position = Vector3.Lerp(_linePoints[_pathIndex-1], _linePoints[_pathIndex], _moveT / totalSegmentTime);
 
 		if (Mathf.Approximately(Vector3.Distance(_linePoints[_pathIndex], transform.position), 0)) return;
 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_linePoints[_pathIndex] - _linePoints[_pathIndex-1]), 10f * Time.deltaTime);
+	}
+
+	private void MoveFreely()
+	{
+		transform.Translate(transform.forward * _speed * Time.deltaTime, Space.World);
+
+		if (!Mathf.Approximately(_rotationRemaining, 0))
+		{
+			float sign = Mathf.Sign(_rotationRemaining);
+			Vector3 rotation = new Vector3(0, sign *70f * Time.deltaTime * 2, 0); // make 70 into seriaz variable
+			transform.Rotate(rotation, Space.Self);
+
+			_rotationRemaining -= sign * 70f * Time.deltaTime * 2;
+			
+			if ((sign > 0 && _rotationRemaining < 0) || (sign < 0 && _rotationRemaining > 0))
+				_rotationRemaining = 0;
+			return;
+		}
+		
+		
+		// move forward
+		// 2 raycasts, 45 front left, front right
+		// turn rate depending on depth?
+		Quaternion rotLeft = transform.rotation * Quaternion.Euler(0, 45, 0);
+		Vector3 dirLeft = rotLeft * Vector3.forward;
+		float distLeft = GetWallDistance(dirLeft);
+
+		Quaternion rotRight = transform.rotation * Quaternion.Euler(0, -45, 0);
+		Vector3 dirRight = rotRight * Vector3.forward;
+		float distRight = GetWallDistance(dirRight);
+
+		float dist = (distLeft < distRight) ? distLeft : distRight;
+		if (dist < _wallCheckDist)
+		{
+			// rotate 
+			// transform.Rotate(Vector3.up, ((distLeft < distRight) ? -1 : 1 * _rotationSpeed) * distLeft * Time.deltaTime);
+			_rotationRemaining = ((distLeft < distRight) ? -1 : 1) * 70f;
+		}
+		
+		// TODO new system - baby keeps rotating after finding wall
+		// start a random rotation for a second
+		// keep track in _rotationRemaining
+	}
+
+	private float GetWallDistance(Vector3 direction)
+	{
+		Debug.DrawRay(transform.position + Vector3.up*0.2f, direction*1000, Color.red);
+
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, direction, out hit, 100, 1 << LayerMask.NameToLayer("Wall")))
+		{
+			return hit.distance;
+		}
+	
+		return -1;
 	}
 }
